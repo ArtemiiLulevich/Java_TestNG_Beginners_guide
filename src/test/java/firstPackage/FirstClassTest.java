aaba2c9571e57645a318379f4ba6b90705b18434
Artemii <artemii.lulevich@gmail.com>
2022-04-29 16:23:12 +0300

    Add packageOne

---
+++ src/test/java/firstPackage/FirstClassTest.java
@@ -0,0 +1,12 @@
+package firstPackage;
+
+import org.testng.annotations.Test;
+
+public class FirstClassTest {
+
+    @Test
+    public void firstTest() {
+        System.out.println("First test method.");
+    }
+
+}
---
+++ src/test/java/firstPackage/SecondClassTest.java
@@ -0,0 +1,12 @@
+package firstPackage;
+
+import org.testng.annotations.Test;
+
+public class SecondClassTest {
+
+    @Test
+    public void secondTest() {
+        System.out.println("Second test method.");
+    }
+
+}
